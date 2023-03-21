6cccc7b861416ea3cf1b8d9226405742798315ed
Yavzkesta <48090660+Yavzkesta@users.noreply.github.com>
2023-03-21 22:46:24 +0100

    Update slovene.php

--- locale/slovene.php
+++ locale/slovene.php
@@ -85,3 +85,117 @@ $_LANG['readytostartdesc'] = "Strežniki vedno obratujejo in delujejo, zato post
 $_LANG['lookservice'] = "Ogled Storitev";
 $_LANG['espaceclient'] = "Področje za Stranke";
 $_LANG['createaccountweb'] = "Ustvari Račun";
+
+// homepage
+$_LANG['seoenhancer_homepage_title'] = "Gostovanje igralnega strežnika | Hebergtonserv.fr";
+$_LANG['seoenhancer_homepage_description'] = "Hebergtonserv.fr je podjetje, specializirano za gostovanje strežnikov za igre. Ponujamo celotno paleto rešitev gostovanja za najbolj priljubljene igre, vključno z Minecraftom, Garry's Mod, GTA FiveM in številnimi drugimi.";
+$_LANG['seoenhancer_homepage_tags'] = "Gostovanje strežnikov, spletne igre, strežniki Minecraft, strežniki FiveM, strežniki Garry's Mod";
+$_LANG['seoenhancer_homepage_canonical'] = "https://hebergtonserv.fr/";
+
+// fivem
+$_LANG['seoenhancer_2_title'] = "Hitro in zanesljivo Gostovanje strežnikov FiveM - HebergTonServ.fr";
+$_LANG['seoenhancer_2_description'] = "HebergTonServ.fr ponuja hitre in zanesljive rešitve za gostovanje strežnikov FiveM po dobrih cenah. Nastavite in upravljajte svoj strežnik z nekaj kliki z našo prilagodljivo nadzorno ploščo.";
+$_LANG['seoenhancer_2_tags'] = "Gostovanje, strežniki FiveM, konfiguracija, nadzorna plošča, ugodno";
+$_LANG['seoenhancer_2_canonical'] = "https://hebergtonserv.fr/fivem.php";
+
+// minecraft
+$_LANG['seoenhancer_3_title'] = "Visokokakovostno Gostovanje Minecraft strežnika | HebergTonServ";
+$_LANG['seoenhancer_3_description'] = "HebergTonServ ponuja optimizirane pakete gostovanja strežnika Minecraft za nemoteno igralno izkušnjo brez zamikov. Uživajte v številnih strežniških možnostih, vključno z Vanilla, Spigot, Forge, BungeeCord in drugimi. Dobite tudi zaščito pred DDoS, redne varnostne kopije in tehnično podporo 24/7, da zagotovite najboljšo igralno izkušnjo.";
+$_LANG['seoenhancer_3_tags'] = "Gostovanje strežnika Minecraft, Minecraft, igralni strežniki, gostovanje strežnikov, Spigot, Forge, BungeeCord, anti-DDoS, redne varnostne kopije, 24/7 tehnična podpora";
+$_LANG['seoenhancer_3_canonical'] = "https://hebergtonserv.fr/minecraft.php";
+
+// gmod
+$_LANG['seoenhancer_4_title'] = "Vrhunsko kakovostno Gostovanje strežnika Garry's Mod | HebergTonServ";
+$_LANG['seoenhancer_4_description'] = "Gostite svoj strežnik Garry's Mod s HebergTonServ. Uživajte v gostovanju vrhunske kakovosti z najsodobnejšimi procesorji, samodejnimi varnostnimi kopijami, zaščito pred napadi DDoS in tehnično podporo 24/7. Oglejte si naše načrte gostovanja Garry's Mod zdaj!";
+$_LANG['seoenhancer_4_tags'] = "Gostovanje Garry's Mod, strežnik Garry's Mod, vrhunska kakovost, najsodobnejši procesorji, samodejno varnostno kopiranje, DDoS zaščita, tehnična podpora";
+$_LANG['seoenhancer_4_canonical'] = "https://hebergtonserv.fr/garrys-mod.php";
+
+// gameserver
+$_LANG['seoenhancer_gameserver_title'] = "Visoko kakovostno | Gostovanje igralnih strežnikov | HebergTonServ";
+$_LANG['seoenhancer_gameserver_description'] = "HebergTonServ ponuja igralne strežnike vrhunske kakovosti za strastne igričarje. Naši strežniki so optimizirani za zagotavljanje izjemne zmogljivosti in hitre, stabilne povezave. Ponujamo vrsto priljubljenih iger z izjemno tehnično podporo, da vam zagotovimo izkušnjo igranja brez stresa. Zagotovite si svoj vrhunski igralni strežnik še danes!";
+$_LANG['seoenhancer_gameserver_tags'] = "Gostovanje strežnikov za igre, strežniki za igre, strežniki za igre vrhunske kakovosti, Minecraft, Garry's Mod, FiveM, storitve strežnikov za igre, optimizirani strežniki za igre";
+$_LANG['seoenhancer_gameserver_canonical'] = "https://hebergtonserv.fr/game-servers.php";
+
+// vps
+$_LANG['seoenhancer_vps_title'] = "Visoko zmogljivo VPS Gostovanje - HebergTonServ";
+$_LANG['seoenhancer_vps_description'] = "Odkrijte naše visoko zmogljivo gostovanje VPS. Uživajte v zmogljivem korenskem dostopu, izboljšani varnosti in odzivni tehnični podpori.";
+$_LANG['seoenhancer_vps_tags'] = "VPS gostovanje, VPS strežnik, gostovanje v oblaku, root dostop, izboljšana varnost, tehnična podpora, visoka zmogljivost";
+$_LANG['seoenhancer_vps_canonical'] = "https://hebergtonserv.fr/vps-hosting.php";
+
+// web
+$_LANG['seoenhancer_5_title'] = "Premium spletno Gostovanje - HebergTonServ.fr";
+$_LANG['seoenhancer_5_description'] = "HebergTonServ.fr ponuja vrhunske storitve spletnega gostovanja za spletna mesta vseh velikosti. Naši paketi vključujejo shranjevanje NVMe SSD, zaščito pred DDoS, 24/7 tehnično podporo, varnostno kopiranje podatkov in 99,9-odstotno garancijo razpoložljivosti.";
+$_LANG['seoenhancer_5_tags'] = "Spletno gostovanje, NVMe SSD shranjevanje, DDoS zaščita, varnostno kopiranje podatkov, razpoložljivost, tehnična podpora, gostovanje spletne strani";
+$_LANG['seoenhancer_5_canonical'] = "https://hebergtonserv.fr/web-hosting.php";
+
+// legal
+$_LANG['seoenhancer_6_title'] = "Prodajni pogoji - Hébergtonserv.fr";
+$_LANG['seoenhancer_6_description'] = "Oglejte si naše prodajne pogoje, če želite izvedeti več o naših storitvah gostovanja za igralne strežnike in VPS. Poiščite vse informacije o cenah, vračilih, razpoložljivosti, varnosti in zaupnosti vaših podatkov.";
+$_LANG['seoenhancer_6_tags'] = "Gostovanje igralnega strežnika, VPS, prodajni pogoji, cene, povračila, varnost podatkov, zaupnost";
+$_LANG['seoenhancer_6_canonical'] = "https://hebergtonserv.fr/terms-of-service.php";
+
+// gaming
+$_LANG['seoenhancer_7_title'] = "Gostovanje igralnega strežnika - Ustvarite račun pri HebergTonServ";
+$_LANG['seoenhancer_7_description'] = "Ustvarite račun pri HebergTonServ in uživajte v visokokakovostnem gostovanju strežnika iger po dostopnih cenah. Dostopajte do naše ponudbe strežnikov za igre, VPS in spletnega gostovanja, da boste izpolnili vse svoje potrebe.";
+$_LANG['seoenhancer_7_tags'] = "Gostovanje igralnega strežnika, račun, HebergTonServ, igralni strežniki, VPS, spletno gostovanje";
+$_LANG['seoenhancer_7_canonical'] = "https://hebergtonserv.fr/register.php";
+
+// Garry's Mod
+$_LANG['seoenhancer_8_title'] = "Garry's Mod Hosting – Zmogljivi in ​​zanesljivi igralni strežniki | HebergTonServ";
+$_LANG['seoenhancer_8_description'] = "Gostite svoj strežnik Garry's Mod s HebergTonServ in uživajte v vrhunski zmogljivosti, zaščiti proti DDoS ter hitri, profesionalni tehnični podpori. Naši strežniki so optimizirani za Garry's Mod, kar zagotavlja hitro nalaganje in gladko igralno izkušnjo za vas in vaše igralce.";
+$_LANG['seoenhancer_8_tags'] = "Garry's Mod gostovanje, Garry's Mod igralni strežniki, vrhunska zmogljivost, DDoS zaščita, tehnična podpora, nemotena igralna izkušnja";
+$_LANG['seoenhancer_8_canonical'] = "https://hebergtonserv.fr/store/gmod";
+
+// ALT:V
+$_LANG['seoenhancer_9_title'] = "Gostovanje strežnikov ALT:V - Visoko zmogljivi igralni strežniki";
+$_LANG['seoenhancer_9_description'] = "Zagotovite si svoj visoko zmogljiv strežnik ALT:V pri Hébergtonserv.fr. Ponujamo različne strežniške načrte, možnosti prilagajanja, 24/7 tehnično podporo in zaščito pred DDoS za neprekinjeno igralno izkušnjo.";
+$_LANG['seoenhancer_9_tags'] = "Gostovanje strežnikov, igralni strežniki, ALT:V strežniki, visoka zmogljivost, prilagoditev, tehnična podpora, DDoS zaščita.";
+$_LANG['seoenhancer_9_canonical'] = "https://hebergtonserv.fr/store/altv";
+
+// RedM
+$_LANG['seoenhancer_0010_title'] = "Gostovanje strežnikov RedM - HebergtonServ";
+$_LANG['seoenhancer_0010_description'] = "Gostite svoj strežnik RedM s HebergtonServ. Uživajte v visoki zmogljivosti, najsodobnejši zaščiti DDoS, tehnični podpori 24/7 in nadzorni plošči, ki je preprosta za uporabo. Začnite svojo skupnost RedM še danes!";
+$_LANG['seoenhancer_0010_tags'] = "Gostovanje RedM, strežnik RedM, gostovanje RedM, gostovanje igralnega strežnika, visoka zmogljivost, DDoS zaščita, tehnična podpora 24/7, nadzorna plošča, enostavna za uporabo";
+$_LANG['seoenhancer_0010_canonical'] = "https://hebergtonserv.fr/store/redm";
+
+// FiveM
+$_LANG['seoenhancer_0011_title'] = "Kakovostno Gostovanje strežnikov FiveM | HebergTonServ";
+$_LANG['seoenhancer_0011_description'] = "Zagotovite si optimalno zmogljivost za svoj FiveM strežnik z našim visoko zmogljivim gostovanjem in namensko tehnično podporo. Izbirajte med različnimi načrti gostovanja po konkurenčnih cenah. Pridružite se naši skupnosti in uživajte v zanesljivem in varnem gostovanju strežnika FiveM še danes.";
+$_LANG['seoenhancer_0011_tags'] = "Gostovanje, strežnik, FiveM, zmogljivost, podpora, tehnično, skupnost, konkurenčne cene, zanesljivost, varnost";
+$_LANG['seoenhancer_0011_canonical'] = "https://hebergtonserv.fr/store/fivem";
+
+// Node.js
+$_LANG['seoenhancer_0012_title'] = "Vrhunsko Gostovanje Node.js | Gostovanje igralnega strežnika";
+$_LANG['seoenhancer_0012_description'] = "Gostite svojo aplikacijo Node.js z zmogljivostjo in zanesljivostjo v naši infrastrukturi v oblaku. Uživajte v funkcijah, kot so samodejno varnostno kopiranje, popoln korenski dostop in odzivna tehnična podpora.";
+$_LANG['seoenhancer_0012_tags'] = "Gostovanje Node.js, strežnik v oblaku, samodejne varnostne kopije, popoln korenski dostop, tehnična podpora";
+$_LANG['seoenhancer_0012_canonical'] = "https://hebergtonserv.fr/store/nodejs";
+
+// VPS
+$_LANG['seoenhancer_0013_title'] = "Visokokakovostno VPS Gostovanje - Hebergtonserv.fr";
+$_LANG['seoenhancer_0013_description'] = "Zagotovite si zanesljivo delovanje, izboljšano varnost in vrhunsko podporo strankam z našim gostovanjem VPS v oblaku. Odkrijte naše upravljane, razširljive in stroškovno učinkovite načrte VPS s korenskim dostopom, samodejnim varnostnim kopiranjem in vrhunskim omrežjem.";
+$_LANG['seoenhancer_0013_tags'] = "Gostovanje VPS, gostovanje VPS v oblaku, upravljano gostovanje VPS, korenski dostop, samodejne varnostne kopije, premium omrežje, zanesljivo delovanje, izboljšana varnost, vrhunska podpora strankam, stroškovno učinkoviti načrti VPS";
+$_LANG['seoenhancer_0013_canonical'] = "https://hebergtonserv.fr/store/vps";
+
+// Minecraft
+$_LANG['seoenhancer_0014_title'] = "Gostovanje strežnika Minecraft - gostite svoj strežnik Minecraft s HebergTonServ";
+$_LANG['seoenhancer_0014_description'] = "Gostite svoj Minecraft strežnik s HebergTonServ in uživajte v vrhunski zmogljivosti in varnosti. Izbirajte med različnimi načrti gostovanja in z našo uporabniku prijazno nadzorno ploščo preprosto upravljajte svoj strežnik Minecraft.";
+$_LANG['seoenhancer_0014_tags'] = "Gostovanje strežnika Minecraft, strežnik Minecraft, Minecraft, gostovanje iger, HebergTonServ";
+$_LANG['seoenhancer_0014_canonical'] = "https://hebergtonserv.fr/store/minecraft";
+
+// Web Hosting
+$_LANG['seoenhancer_0015_title'] = "Spletno Gostovanje - Ugodni paketi spletnega gostovanja | HebergtonServ";
+$_LANG['seoenhancer_0015_description'] = "Gostite svoje spletno mesto z našimi cenovno ugodnimi načrti spletnega gostovanja. Izkoristite hitro shranjevanje NVMe SSD, redne varnostne kopije, zaščito pred napadi DDoS in tehnično podporo 24/7.";
+$_LANG['seoenhancer_0015_tags'] = "Spletno gostovanje, shramba NVMe SSD, redne varnostne kopije, zaščita DDoS, 24/7 tehnična podpora, cenovno ugodni paketi spletnega gostovanja, HebergtonServ";
+$_LANG['seoenhancer_0015_canonical'] = "https://hebergtonserv.fr/store/web";
+
+// Database Hosting
+$_LANG['seoenhancer_0016_title'] = "Database Gostovanje - HebergTonServ";
+$_LANG['seoenhancer_0016_description'] = "HebergTonServ ponuja hitre in zanesljive rešitve za gostovanje databaze podatkov, ki izpolnjujejo vse vaše potrebe po shranjevanju podatkov. Naši strežniki baz podatkov so optimizirani za zmogljivost in imajo funkcije, kot so samodejno varnostno kopiranje, zaščita pred napadi DDoS ter hitra in kompetentna tehnična podpora. Izbirajte med našimi različnimi načrti in poiščite tistega, ki najbolj ustreza vašim potrebam.";
+$_LANG['seoenhancer_0016_tags'] = "Gostovanje databaz podatkov, MySQL, MariaDB, PostgreSQL, MongoDB, Redis 5, Redis 6, shranjevanje podatkov, samodejne varnostne kopije, DDoS zaščita, tehnična podpora, načrti gostovanja.";
+$_LANG['seoenhancer_0016_canonical'] = "https://hebergtonserv.fr/store/database";
+
+// Gaming VPS
+$_LANG['seoenhancer_0017_title'] = "Gaming VPS za igranje - HebergTonServ";
+$_LANG['seoenhancer_0017_description'] = "HebergTonServ ponuja vrhunske strežnike VPS za spletno igranje z visoko zmogljivostjo, hitro povezljivostjo in 24/7 tehnično podporo.";
+$_LANG['seoenhancer_0017_tags'] = "VPS gostovanje, igralni VPS strežnik, spletni igralni strežnik, visoka zmogljivost, 24/7 tehnična podpora, hitra povezljivost";
+$_LANG['seoenhancer_0017_canonical'] = "https://hebergtonserv.fr/store/vps-gaming";
